0fdcb1f1f33b7a5fd876a16f4ffebf109770c04e
Joe <caspar0912@googlemail.com>
2022-06-03 18:25:45 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function computerPlay () {
+    const random = Math.floor(Math.random() * 3)
+    switch (random) {
+        case 0:
+            return 'rock'
+        case 1:
+            return 'paper'
+        case 2:
+            return 'scissors'
+    }
+
+}
+
+console.log(computerPlay());

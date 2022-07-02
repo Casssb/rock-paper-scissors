751eabc40788c8ca9bcbd0a7db01449846f72a32
Joe <caspar0912@googlemail.com>
2022-07-02 20:35:58 +0100

    updated readme and ui

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rock-paper-scissors
+
+My first proper project via The Odin Project curriculum using vanilla JS, HTML & CSS.
+
+[Check it out here](https://michalosman.github.io/rock-paper-scissors/) :point_left:
--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ let botScore = 0;
 const result = document.querySelector("#roundTally");
 const playerDisplay = document.querySelector("#playerScore");
 const botDisplay = document.querySelector("#botScore");
-const botImagecontainer = document.querySelector(".botImage")
-const botImage = document.createElement('img')
+const botImagecontainer = document.querySelector(".botImage");
+const botImage = document.createElement("img");
 const playerButton = document.querySelectorAll(".playerButton");
 
 //each click triggers a round
 playerButton.forEach((button) =>
   button.addEventListener("click", (e) => {
     playerChoice = e.currentTarget.id;
-    playRound(botPlay(), playerChoice)
+    playRound(botPlay(), playerChoice);
   })
 );
 
@@ -24,38 +24,43 @@ function botPlay() {
   const random = Math.floor(Math.random() * 3);
   switch (random) {
     case 0:
-      botImage.src = 'img/ninja.png';
-      botImagecontainer.appendChild(botImage)
+      botImage.src = "img/ninja.png";
+      botImagecontainer.appendChild(botImage);
       return "ninja";
     case 1:
-      botImage.src = 'img/hunter.png';
-      botImagecontainer.appendChild(botImage)
+      botImage.src = "img/hunter.png";
+      botImagecontainer.appendChild(botImage);
       return "hunter";
     case 2:
-      botImage.src = 'img/bear.png';
-      botImagecontainer.appendChild(botImage)
+      botImage.src = "img/bear.png";
+      botImagecontainer.appendChild(botImage);
       return "bear";
   }
 }
 
 function playRound(botChoice, playerChoice) {
+    result.style.fontSize = "medium";
+    result.style.color = "black";
+    botImagecontainer.style.backgroundColor = 'white';
   if (
     (playerChoice === "ninja" && botChoice === "hunter") ||
     (playerChoice === "hunter" && botChoice === "bear") ||
     (playerChoice === "bear" && botChoice === "ninja")
   ) {
     playerScore++;
+    botImagecontainer.style.backgroundColor = 'mediumseagreen';
     result.innerText = `You win! ${playerChoice} beats ${botChoice}`;
   } else if (playerChoice === botChoice) {
+    botImagecontainer.style.backgroundColor = 'goldenrod';
     result.innerText = "It's a draw";
   } else {
     botScore++;
+    botImagecontainer.style.backgroundColor = 'crimson';
     result.innerText = `You lose! ${botChoice} beats ${playerChoice}`;
   }
   updateScore();
   gameOverMan();
 }
-
 
 //update & reset DOM based on scores
 
@@ -66,10 +71,14 @@ function updateScore() {
 
 function gameOverMan() {
   if (playerScore >= 5) {
+    result.style.fontSize = "large";
+    result.style.color = "seagreen";
     result.innerText = `You win the game!!! ${playerScore}:${botScore}. Chose again to start a new game`;
     resetScore();
   } else if (botScore >= 5) {
-    result.innerText = `You lose the game!!! ${botScore}:${playerScore}. Chose again to start a new game`;
+    result.style.fontSize = "large";
+    result.style.color = "crimson";
+    result.innerText = `Oops you lost! ${botScore}:${playerScore}. Chose again to start a new game`;
     resetScore();
   }
 }
